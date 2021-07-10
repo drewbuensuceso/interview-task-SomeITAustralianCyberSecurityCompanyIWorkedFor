890538c93a0a0a1df430eea886f760c0bdeb50d7
drewbuensuceso <rayandrew.buensuceso@gmail.com>
2021-07-10 18:54:43 +0800

    -added catche exception for Check_ES_Conn

--- ES_Manager.py
+++ ES_Manager.py
@@ -1,5 +1,5 @@
 import os
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, RequestError
 from config import ElasticSearchDetails
 
 ESDetails = ElasticSearchDetails()
@@ -11,10 +11,14 @@ class ESM():
         self.password = ESDetails.password
         self.host = ESDetails.host
         self.port = ESDetails.port
+        self.error = None
 
     def Check_ES_Conn(self):
-        self.es = Elasticsearch(['https://{user}:{password}@{host}:{port}'.format(
-            user=self.username, password=self.password, host=self.host, port=self.port)])
+        try:
+            self.es = Elasticsearch(['https://{user}:{password}@{host}:{port}'.format(
+                user=self.username, password=self.password, host=self.host, port=self.port)])
+        except RequestError as e:
+            self.error = e
         return self
 
     def Search_Result(self, query, index):

b91d98c3a6ad783a82a844f4720596dfe4d79bf7
drewbuensuceso <rayandrew.buensuceso@gmail.com>
2021-07-11 23:05:08 +0800

    added filter path for Search_Result

--- ES_Manager.py
+++ ES_Manager.py
@@ -17,12 +17,12 @@ class ESM():
         try:
             self.es = Elasticsearch(['https://{user}:{password}@{host}:{port}'.format(
                 user=self.username, password=self.password, host=self.host, port=self.port)])
-            print("Connected Successfully!", self.es.info())
+            #print("Connected Successfully!", self.es.info())
         except Exception as e:
-            print("Connection Failed", e)
+            #print("Connection Failed", e)
             self.error = e
         return self
 
     def Search_Result(self, query, index):
-        self.response = self.es.search(index=index, body=query)
+        self.response = self.es.search(index=index, body=query, filter_path=['hits.*.value','aggregations.*.buckets'])
         return self.response
